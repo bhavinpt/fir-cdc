861c03adbbce95281a94605002bb5ff8c2fce7a3
Bhavin Patel <117598876+bhavinpt@users.noreply.github.com>
2023-10-06 16:58:48 -0700

    Indented firc.v

--- source/firc.v
+++ source/firc.v
@@ -37,40 +37,24 @@ module firc(input wire Clk,
             input wire [`S_WIDTH-1:0] SampI,
             input wire [`S_WIDTH-1:0] SampQ,
             input wire PushCoef,
-            input wire [$clog2(`N_TAPS)-1:0] CoefAddr, input wire [`C_WIDTH-1:0] CoefI, input wire [`C_WIDTH-1:0] CoefQ, output wire PushOut, output wire [`R_WIDTH-1:0] FI, output wire [`R_WIDTH-1:0] FQ);
-    
-    
+            input wire [$clog2(`N_TAPS)-1:0] CoefAddr, 
+            input wire [`C_WIDTH-1:0] CoefI, 
+            input wire [`C_WIDTH-1:0] CoefQ, 
+            output wire PushOut, 
+            output wire [`R_WIDTH-1:0] FI, 
+            output wire [`R_WIDTH-1:0] FQ
+           );
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-    //initial begin
-    //    $dumpfile("waves.vcd");
-    //    $dumpvars(0);
-    //end
+  wire busy_mux;
+  wire last_mux_group; // mux gave out the last group 
+  wire canShift = last_mux_group;
+  reg sampleEmpty;
 
-    //integer poutCount = 0;
-    //always@(posedge PushOut) begin
-    //    $display("pushout, sample %d  @%t", ++poutCount, $time); 
-    //end
-
-    //integer stopCount = 0;
-    //always@(negedge StopIn) begin
-    //    $display("stopIn, sample %d  @%t", ++stopCount, $time); 
-    //end
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-    wire busy_mux;
-    wire last_mux_group; // mux gave out the last group 
-    wire canShift = last_mux_group;
-    reg sampleEmpty;
-    
-    // clk 1 -> enters FIFO
-    // clk 2 -> enters shiftreg
-    wire [(`S_WIDTH*`N_TAPS)-1:0] sampBuffI;
-    wire [(`S_WIDTH*`N_TAPS)-1:0] sampBuffQ;
-    Sample_Fetch #(.S_WIDTH(`S_WIDTH), .S_DEPTH(`N_TAPS)) sample_fetch(
+  // clk 1 -> enters FIFO
+  // clk 2 -> enters shiftreg
+  wire [(`S_WIDTH*`N_TAPS)-1:0] sampBuffI;
+  wire [(`S_WIDTH*`N_TAPS)-1:0] sampBuffQ;
+  Sample_Fetch #(.S_WIDTH(`S_WIDTH), .S_DEPTH(`N_TAPS)) sample_fetch(
     .clk(Clk),
     .reset(Reset),
     .incoming(PushIn&~StopIn),
@@ -81,116 +65,134 @@ module firc(input wire Clk,
     .shiftRegQ(sampBuffQ),
     .stopOut(StopIn),
     .empty(sampleEmpty)
-    );
-    
-    wire [`C_WIDTH*`N_TAPS_HALF-1:0] coefBuffI;
-    Coef_Buffer #(.WIDTH(`C_WIDTH), .DEPTH(`N_TAPS_HALF)) coef_buffer_I(
+  );
+
+  wire [`C_WIDTH*`N_TAPS_HALF-1:0] coefBuffI;
+  Coef_Buffer #(.WIDTH(`C_WIDTH), .DEPTH(`N_TAPS_HALF)) coef_buffer_I(
     .clk(Clk),
     .reset(Reset),
     .load(PushCoef),
     .loadidx(CoefAddr),
     .datain(CoefI),
     .dataout(coefBuffI)
-    );
-    
-    wire [`C_WIDTH*`N_TAPS_HALF-1:0] coefBuffQ;
-    Coef_Buffer #(.WIDTH(`C_WIDTH), .DEPTH(`N_TAPS_HALF)) coef_buffer_Q(
+  );
+
+  wire [`C_WIDTH*`N_TAPS_HALF-1:0] coefBuffQ;
+  Coef_Buffer #(.WIDTH(`C_WIDTH), .DEPTH(`N_TAPS_HALF)) coef_buffer_Q(
     .clk(Clk),
     .reset(Reset),
     .load(PushCoef),
     .loadidx(CoefAddr),
     .datain(CoefQ),
     .dataout(coefBuffQ)
-    );
-    
-    wire [(`S_WIDTH*`C_PAIR_SIZE)-1:0] sampMuxI; // intentionally C_PAIR
-    wire [(`S_WIDTH*`C_PAIR_SIZE)-1:0] sampMuxQ; // intentionally C_PAIR
-    wire [(`C_WIDTH*`C_PAIR_SIZE)-1:0] coefMuxI;
-    wire [(`C_WIDTH*`C_PAIR_SIZE)-1:0] coefMuxQ;
+  );
 
-    Mux_Controller 
-    #(.C_WIDTH(`C_WIDTH),
-        .S_WIDTH(`S_WIDTH),
-        .C_DEPTH(`N_TAPS_HALF),
-        .S_DEPTH(`N_TAPS),
-        .S_MUXES(`S_PAIR_SIZE), 
-        .C_MUXES(`C_PAIR_SIZE))
-        mux_controller(
-            .clk(Clk),
-            .reset(Reset),
-            .empty(sampleEmpty),
-            .coef_inI(coefBuffI),
-            .coef_inQ(coefBuffQ),
-            .samp_inI(sampBuffI),
-            .samp_inQ(sampBuffQ),
-            .coef_outI(coefMuxI),
-            .coef_outQ(coefMuxQ),
-            .samp_outI(sampMuxI),
-            .samp_outQ(sampMuxQ),
-            .busy(busy_mux),
-            .last(last_mux_group)
-        );
+  wire [(`S_WIDTH*`C_PAIR_SIZE)-1:0] sampMuxI; // intentionally C_PAIR
+  wire [(`S_WIDTH*`C_PAIR_SIZE)-1:0] sampMuxQ; // intentionally C_PAIR
+  wire [(`C_WIDTH*`C_PAIR_SIZE)-1:0] coefMuxI;
+  wire [(`C_WIDTH*`C_PAIR_SIZE)-1:0] coefMuxQ;
 
-        // clk 3 -> enters cmul
-        wire [`S_WIDTH+`C_WIDTH:0] r0I, r1I, r2I, r3I, r4I;
-        wire [`S_WIDTH+`C_WIDTH:0] r0Q, r1Q, r2Q, r3Q, r4Q;
-        wire busy_cmul;
-        CompMul_Array #(.S_WIDTH(`S_WIDTH), .C_WIDTH(`C_WIDTH), .MUXES(`C_PAIR_SIZE)) compmul_array
-        (
-            .clk(Clk),
-            .reset(Reset),
-            .busy_in(last_mux_group& PushIn),
-            .coef_inI(coefMuxI),
-            .coef_inQ(coefMuxQ),
-            .samp_inI(sampMuxI),
-            .samp_inQ(sampMuxQ),
+  Mux_Controller 
+  #(.C_WIDTH(`C_WIDTH),
+    .S_WIDTH(`S_WIDTH),
+    .C_DEPTH(`N_TAPS_HALF),
+    .S_DEPTH(`N_TAPS),
+    .S_MUXES(`S_PAIR_SIZE), 
+    .C_MUXES(`C_PAIR_SIZE))
+  mux_controller(
+    .clk(Clk),
+    .reset(Reset),
+    .empty(sampleEmpty),
+    .coef_inI(coefBuffI),
+    .coef_inQ(coefBuffQ),
+    .samp_inI(sampBuffI),
+    .samp_inQ(sampBuffQ),
+    .coef_outI(coefMuxI),
+    .coef_outQ(coefMuxQ),
+    .samp_outI(sampMuxI),
+    .samp_outQ(sampMuxQ),
+    .busy(busy_mux),
+    .last(last_mux_group)
+  );
 
-            .r0I(r0I),
-            .r1I(r1I),
-            .r2I(r2I),
-            .r3I(r3I),
-            .r4I(r4I),
+  // clk 3 -> enters cmul
+  wire [`S_WIDTH+`C_WIDTH:0] r0I, r1I, r2I, r3I, r4I;
+  wire [`S_WIDTH+`C_WIDTH:0] r0Q, r1Q, r2Q, r3Q, r4Q;
+  wire busy_cmul;
+  CompMul_Array #(.S_WIDTH(`S_WIDTH), .C_WIDTH(`C_WIDTH), .MUXES(`C_PAIR_SIZE)) compmul_array
+  (
+    .clk(Clk),
+    .reset(Reset),
+    .busy_in(last_mux_group& PushIn),
+    .coef_inI(coefMuxI),
+    .coef_inQ(coefMuxQ),
+    .samp_inI(sampMuxI),
+    .samp_inQ(sampMuxQ),
 
-            .r0Q(r0Q),
-            .r1Q(r1Q),
-            .r2Q(r2Q),
-            .r3Q(r3Q),
-            .r4Q(r4Q),
+    .r0I(r0I),
+    .r1I(r1I),
+    .r2I(r2I),
+    .r3I(r3I),
+    .r4I(r4I),
 
-            .busy_out(busy_cmul)
-        );
+    .r0Q(r0Q),
+    .r1Q(r1Q),
+    .r2Q(r2Q),
+    .r3Q(r3Q),
+    .r4Q(r4Q),
 
-        // clk 7 -> enters acc 
-        wire [55:0] finalOutI, finalOutQ;
-        assign FI = finalOutI[55:(55-32)];
-        assign FQ = finalOutQ[55:(55-32)];
-        Accumulator accmulator(
-            .clk(Clk),
-            .reset(Reset),
-            .valid(busy_cmul),
+    .busy_out(busy_cmul)
+  );
 
-            .r0I(r0I),
-            .r1I(r1I),
-            .r2I(r2I),
-            .r3I(r3I),
-            .r4I(r4I),
+  // clk 7 -> enters acc 
+  wire [55:0] finalOutI, finalOutQ;
+  assign FI = finalOutI[55:(55-32)];
+  assign FQ = finalOutQ[55:(55-32)];
+  Accumulator accmulator(
+    .clk(Clk),
+    .reset(Reset),
+    .valid(busy_cmul),
 
-            .r0Q(r0Q),
-            .r1Q(r1Q),
-            .r2Q(r2Q),
-            .r3Q(r3Q),
-            .r4Q(r4Q),
+    .r0I(r0I),
+    .r1I(r1I),
+    .r2I(r2I),
+    .r3I(r3I),
+    .r4I(r4I),
 
-            .pushOut(PushOut),
-            .finalOutI(finalOutI),
-            .finalOutQ(finalOutQ)
-        );
+    .r0Q(r0Q),
+    .r1Q(r1Q),
+    .r2Q(r2Q),
+    .r3Q(r3Q),
+    .r4Q(r4Q),
 
-        // clk 10 -> output
+    .pushOut(PushOut),
+    .finalOutI(finalOutI),
+    .finalOutQ(finalOutQ)
+  );
 
-    
-    
+  // clk 10 -> output
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  //initial begin
+  //    $dumpfile("waves.vcd");
+  //    $dumpvars(0);
+  //end
+
+  //integer poutCount = 0;
+  //always@(posedge PushOut) begin
+  //    $display("pushout, sample %d  @%t", ++poutCount, $time); 
+  //end
+
+  //integer stopCount = 0;
+  //always@(negedge StopIn) begin
+  //    $display("stopIn, sample %d  @%t", ++stopCount, $time); 
+  //end
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
 endmodule
-    
-    
-    `endif
+
+
+`endif
